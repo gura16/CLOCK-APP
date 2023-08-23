606b7f2ee90c9936a51e3ab6653346906953c067
guramikvakhadze <gkvaxadze@gmail.com>
2023-08-23 21:41:37 +0100

    add typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,114 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import refresh from "./image/icon-refresh.svg";
 import sun from "./image/icon-sun.svg";
+import moon from "./image/icon-moon.svg";
 import down from "./image/icon-arrow-down.svg";
 
+function getWeekNumber(date: Date): number {
+  const onejan = new Date(date.getFullYear(), 0, 1);
+  return Math.ceil(
+    ((date.getTime() - onejan.getTime()) / 86400000 + onejan.getDay() + 1) / 7
+  );
+}
+
 function App() {
+  const defaultQuote =
+    "“The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”";
+
   const [localTime, setLocalTime] = useState(new Date());
+  const [text, setText] = useState(defaultQuote);
+  const [author, setAuthor] = useState("Ada Lovelace");
+  const [button, setButton] = useState(true);
+  const [morningOrNight, setMorningOrNight] = useState(true);
+
+  const buttonClick = () => {
+    if (button) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setLocalTime(new Date());
     }, 1000);
 
+    const currentHour = localTime.getHours();
+    if (currentHour >= 6 && currentHour < 18) {
+      setMorningOrNight(true);
+      document.body.className = "";
+    } else {
+      setMorningOrNight(false);
+      document.body.className = "night";
+    }
+
     return () => {
       clearInterval(intervalId);
     };
-  }, []);
+  }, [localTime]);
 
   const formattedLocalTime = localTime.toLocaleTimeString([], {
     hour: "2-digit",
     minute: "2-digit",
   });
 
+  const getTimeZoneParts = () => {
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const [area, location] = timeZone.split("/");
+    return { area, location };
+  };
+
+  const getCurrentDayOfYear = (): number => {
+    const startOfYear = new Date(localTime.getFullYear(), 0, 1);
+    const diff =
+      (localTime.getTime() - startOfYear.getTime()) / (24 * 60 * 60 * 1000);
+    return Math.floor(diff) + 1;
+  };
+
+  const getCurrentDayOfWeek = (): number => {
+    return localTime.getDay();
+  };
+
+  const getCurrentWeek = (): number => {
+    return getWeekNumber(localTime);
+  };
+
+  async function getRandomQuote() {
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
+      return data;
+    } catch (error) {}
+  }
+
+  const fetchRandomQuote = async () => {
+    const randomQuote = await getRandomQuote();
+    setText(randomQuote.content);
+    setAuthor(randomQuote.author);
+  };
+
+  const refreshtext = () => {
+    fetchRandomQuote();
+  };
+
   return (
     <div className="APP">
-      <div className="container1">
+      <div
+        style={button ? { display: "" } : { display: "none" }}
+        className="container1"
+      >
         <h1>
-          “The science of operations, as derived from mathematics more
-          especially, is a science of itself, and has its own abstract truth and
-          value.” <br></br> Ada Lovelace
+          {text} <br></br> {author}
         </h1>
-        <img className="refresh" src={refresh} />
+        <img onClick={refreshtext} className="refresh" src={refresh} />
       </div>
       <div className="container2">
         <div className="morningdiv">
-          <img className="sun" src={sun} />
+          <img className="sun" src={morningOrNight ? sun : moon} />
           <h2>
-            GOOD MORNING <span className="currently">IT’S CURRENTLY</span>
+            GOOD {morningOrNight ? "MORNING" : "EVENING"},{" "}
+            <span className="currently">IT’S CURRENTLY</span>
           </h2>
         </div>
         <div className="timediv">
@@ -44,35 +117,88 @@ function App() {
           <p className="bst">BST</p>
         </div>
         <div className="infodiv">
-          <h3>IN LONDON, UK</h3>
+          <h3>
+            {getTimeZoneParts().area}, {getTimeZoneParts().location}
+          </h3>
           <div className="morediv">
-            <p className="more">MORE</p>
-            <div className="pathdiv">
-              <img className="down" src={down} />
+            <p className="more">{button ? "MORE" : "LESS"}</p>
+            <div onClick={buttonClick} className="pathdiv">
+              <img
+                style={
+                  button
+                    ? { rotate: "", transition: "1.5s" }
+                    : { rotate: "180deg", transition: "1.5s" }
+                }
+                className="down"
+                src={down}
+              />
             </div>
           </div>
         </div>
       </div>
-      <div className="container3">
+      <div
+        style={button ? { display: "none" } : { display: "" }}
+        className={morningOrNight ? "container3" : "container3 night2"}
+      >
         <div className="firstdiv">
           <div className="timezonediv">
-            <p className="times">CURRENT TIMEZONE</p>
-            <p className="numbers">Europe/London</p>
+            <p
+              className="times"
+              style={morningOrNight ? { color: "" } : { color: "white" }}
+            >
+              CURRENT TIMEZONE
+            </p>
+            <p
+              className="numbers"
+              style={morningOrNight ? { color: "" } : { color: "white" }}
+            >
+              {getTimeZoneParts().area}/{getTimeZoneParts().location}
+            </p>
           </div>
           <div className="dayyeardiv">
-            <p className="times">Day of the year</p>
-            <p className="numbers">295</p>
+            <p
+              className="times"
+              style={morningOrNight ? { color: "" } : { color: "white" }}
+            >
+              Day of the year
+            </p>
+            <p
+              className="numbers"
+              style={morningOrNight ? { color: "" } : { color: "white" }}
+            >
+              {getCurrentDayOfYear()}
+            </p>
           </div>
         </div>
         <hr className="hr"></hr>
         <div className="seconddiv">
           <div className="dayweekdiv">
-            <p className="times">Day of the week</p>
-            <p className="numbers">5</p>
+            <p
+              className="times"
+              style={morningOrNight ? { color: "" } : { color: "white" }}
+            >
+              Day of the week
+            </p>
+            <p
+              className="numbers"
+              style={morningOrNight ? { color: "" } : { color: "white" }}
+            >
+              {getCurrentDayOfWeek()}
+            </p>
           </div>
           <div className="numberdiv">
-            <p className="times">Week number</p>
-            <p className="numbers">42</p>
+            <p
+              className="times"
+              style={morningOrNight ? { color: "" } : { color: "white" }}
+            >
+              Week number
+            </p>
+            <p
+              className="numbers"
+              style={morningOrNight ? { color: "" } : { color: "white" }}
+            >
+              {getCurrentWeek()}
+            </p>
           </div>
         </div>
       </div>
